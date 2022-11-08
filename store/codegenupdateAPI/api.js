da9eb57578b7429818ce2cf5204a14bac7a7aa68
Crowdbotics <engineering@crowdbotics.com>
2022-11-08 22:05:31 +0000

    Update store.

---
+++ store/codegenupdateAPI/api.js
@@ -0,0 +1,65 @@
+import axios from "axios"
+const codegenupdateAPI = axios.create({
+  baseURL: "https://codegen-update-1-prod.herokuapp.com",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function rest_auth_login_create(payload) {
+  return codegenupdateAPI.post(`/rest-auth/login/`, payload.data)
+}
+function rest_auth_password_change_create(payload) {
+  return codegenupdateAPI.post(`/rest-auth/password/change/`, payload.data)
+}
+function rest_auth_logout_create(payload) {
+  return codegenupdateAPI.post(`/rest-auth/logout/`)
+}
+function rest_auth_logout_list(payload) {
+  return codegenupdateAPI.get(`/rest-auth/logout/`)
+}
+function rest_auth_password_reset_confirm_create(payload) {
+  return codegenupdateAPI.post(
+    `/rest-auth/password/reset/confirm/`,
+    payload.data
+  )
+}
+function api_v1_login_create(payload) {
+  return codegenupdateAPI.post(`/api/v1/login/`)
+}
+function api_v1_signup_create(payload) {
+  return codegenupdateAPI.post(`/api/v1/signup/`, payload.data)
+}
+function rest_auth_user_partial_update(payload) {
+  return codegenupdateAPI.patch(`/rest-auth/user/`, payload.data)
+}
+function rest_auth_user_read(payload) {
+  return codegenupdateAPI.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(payload) {
+  return codegenupdateAPI.put(`/rest-auth/user/`, payload.data)
+}
+function rest_auth_registration_verify_email_create(payload) {
+  return codegenupdateAPI.post(
+    `/rest-auth/registration/verify-email/`,
+    payload.data
+  )
+}
+function rest_auth_password_reset_create(payload) {
+  return codegenupdateAPI.post(`/rest-auth/password/reset/`, payload.data)
+}
+function rest_auth_registration_create(payload) {
+  return codegenupdateAPI.post(`/rest-auth/registration/`, payload.data)
+}
+export const apiService = {
+  rest_auth_login_create,
+  rest_auth_password_change_create,
+  rest_auth_logout_create,
+  rest_auth_logout_list,
+  rest_auth_password_reset_confirm_create,
+  api_v1_login_create,
+  api_v1_signup_create,
+  rest_auth_user_partial_update,
+  rest_auth_user_read,
+  rest_auth_user_update,
+  rest_auth_registration_verify_email_create,
+  rest_auth_password_reset_create,
+  rest_auth_registration_create
+}
